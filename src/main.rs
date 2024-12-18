15f39b12cc0d76eb330804d957b5580ca5af4ebc
AHN JONG JAE <jjahn504@gmail.com>
2024-12-18 18:57:02 +0900

    v2
    Ok(), Err()

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io::{self, Read};
+
 fn main() {
-    println!("Hello, world!");
+	let mut input = String::new();
+	println!("Please enter some text:");
+	match io::stdin().read_line(&mut input) {
+		Ok(num_of_characters) => {
+			println!("You typed: {}", input.trim());
+			println!("num_of_characters: {}", num_of_characters);
+		},
+		Err(error) => println!("Error reading from stdin: {}", error),
+	}
 }
